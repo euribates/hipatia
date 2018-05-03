3b37900a0c573da4e19673be28fad1928af06018
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2018-05-04 00:17:47 +0100

    Añadido comando make_index

--- web/main/settings.py
+++ web/main/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Whoosh settings
+
+INDEX_DIR = os.path.join(BASE_DIR, 'index')
